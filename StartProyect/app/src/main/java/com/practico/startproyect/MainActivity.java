80c81126519e2caafd207fa5d0ed4f0452c363db
leonnet86 <65618592+leonnet86@users.noreply.github.com>
2024-06-06 23:31:05 -0500

    mod login2
    mod login2

--- StartProyect/app/src/main/java/com/practico/startproyect/MainActivity.java
+++ StartProyect/app/src/main/java/com/practico/startproyect/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button button = findViewById(R.id.btn_ingresar);
             button.setOnClickListener(v -> {
-                
+                Toast.makeText(this,"Ingrese las credenciales",Toast.LENGTH_LONG);
             });
     }
 }
